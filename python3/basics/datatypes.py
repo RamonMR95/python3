d62d84f38283a84a192cca5c39bbf8db85b7ff22
Ramón Moñino Rubio <ramonmr16@gmail.com>
2019-06-22 16:44:21 +0200

    INITIAL COMMIT

---
+++ python3/basics/datatypes.py
@@ -0,0 +1,42 @@
+# INT
+a = 13
+print(type(a))
+
+# FLOAT
+b = 33.2
+print(a, b)
+print(type(b))
+
+# COMPLEX
+c = 3 + 5j
+print(c)
+print(type(c))
+
+# BINARY
+e = 0B1010
+print(e)
+print(type(e))
+
+# HEXADECIMAL
+f = 0XFF
+print(f)
+print(type(f))
+
+# BOOLEAN
+g = True
+print(g)
+print(type(g))
+
+# CASTING
+h = int(b)
+print(h)
+print(type(h))
+
+i = float("22.5")
+print(i)
+print(type(i))
+
+print(bin(10))
+print(hex(10))
+
+
